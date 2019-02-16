8536769edaeb798efae43b494b2bb7a37d37fb35
Hugo Guillín <hugoguillin@hotmail.com>
2019-02-16 19:00:47 +0100

    Task_3

--- app/src/main/java/com/hugoguillin/recyclerview/MainActivity.java
+++ app/src/main/java/com/hugoguillin/recyclerview/MainActivity.java
@@ -30,8 +30,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                int wordListSize = mWordList.size();
+                mWordList.add("+ Letra " + wordListSize);
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                mRecyclerView.smoothScrollToPosition(wordListSize);
             }
         });
 
--- app/src/main/java/com/hugoguillin/recyclerview/WordListAdapter.java
+++ app/src/main/java/com/hugoguillin/recyclerview/WordListAdapter.java
@@ -39,7 +39,8 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
         return mWordList.size();
     }
 
-    class WordViewHolder extends RecyclerView.ViewHolder{
+    class WordViewHolder extends RecyclerView.ViewHolder
+                                implements View.OnClickListener {
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
 
@@ -48,6 +49,15 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = mAdapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int mPosition = getLayoutPosition();
+            String element = mWordList.get(mPosition);
+            mWordList.set(mPosition, "Pulsado! " + element);
+            mAdapter.notifyDataSetChanged();
         }
     }
 }
