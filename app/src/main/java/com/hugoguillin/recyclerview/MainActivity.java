8e5cf268c95d5d0e3c6c04a8673ebb04fb3e59cb
Hugo Guillín <hugoguillin@hotmail.com>
2019-02-16 18:46:56 +0100

    Task_2

--- app/src/main/java/com/hugoguillin/recyclerview/MainActivity.java
+++ app/src/main/java/com/hugoguillin/recyclerview/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -12,6 +14,9 @@ import android.view.MenuItem;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
 
     private final LinkedList<String> mWordList = new LinkedList<>();
     @Override
@@ -33,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++){
             mWordList.add("Letra "+i);
         }
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
---
+++ app/src/main/java/com/hugoguillin/recyclerview/WordListAdapter.java
@@ -0,0 +1,53 @@
+package com.hugoguillin.recyclerview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    public WordListAdapter(Context context, LinkedList<String> mWordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = mWordList;
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View mItemView = mInflater.inflate(R.layout.wordlist_item, viewGroup,
+                false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder wordViewHolder, int i) {
+        String mCurrent = mWordList.get(i);
+        wordViewHolder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder{
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+
+        public WordViewHolder(@NonNull View itemView, WordListAdapter mAdapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = mAdapter;
+        }
+    }
+}
